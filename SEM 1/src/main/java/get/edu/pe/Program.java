d376d4228a53d982686d68727752b8d41c4a675e
Carlos Alvarado <u201916826@upc.edu.pe>
2022-03-25 20:43:28 -0500

    Creación de una clase y objeto v.2

--- SEM 1/src/main/java/get/edu/pe/Program.java
+++ SEM 1/src/main/java/get/edu/pe/Program.java
@@ -2,4 +2,13 @@ package get.edu.pe;
 
 public class Program {
 
+	public static void main(String[] arg) {
+		Student student= new Student();
+		
+		student.setFirstName("Carlos Alonso Alvarado Castañeda");
+		student.setId(1);
+		
+	System.out.println(student);
+		
+	}
 }
